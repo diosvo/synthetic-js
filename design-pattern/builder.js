2a6495578613c922c872ebfc89d454ab1800cf23
Dios Vo <vtmn1212@gmail.com>
2022-03-15 23:07:18 +0700

    design-pattern: builder

---
+++ design-pattern/builder.js
@@ -0,0 +1,23 @@
+class Address {
+  constructor(zip, street) {
+    this.zip = zip;
+    this.street = street;
+  }
+}
+
+class User {
+  // optional properties
+
+  constructor(name, { age, phone, address } = {}) {
+    this.name = name;
+    this.age = age;
+    this.phone = phone;
+    this.address = address;
+  }
+}
+
+console.table([
+  new User("Dios"),
+  new User("Dios", { age: 23, address: new Address("0", "AuCo") }),
+  new User("Dios", { phone: 0927, address: new Address("0", "AuCo") }),
+]);
