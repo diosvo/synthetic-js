53efcf0a248fcf55095add93ab5657b8b0b8bc04
Dios Vo <vtmn1212@gmail.com>
2022-04-07 21:45:37 +0700

    design-pattern (facade)
    - use currying

--- design-pattern/facade/weather-ip-info-async-await.js
+++ design-pattern/facade/weather-ip-info-async-await.js
@@ -1,30 +1,26 @@
 const WEATHER_API_KEY = "75d0116a26d6c8be66e9d67612354e27";
 const IP_INFO_API_KEY = "adec6251a9f09e";
 
-async function fetchData(endpoint, query) {
+const fetchData = (endpoint) => async (query) => {
   const response = await fetch(`${endpoint}?${query}`);
 
   if (!response.ok) {
     throw new Error(`${response.statusText} (${response.status})`);
   }
   return response.json();
-}
-
-async function fetchWeather(city) {
-  const endpoint = "https://api.openweathermap.org/data/2.5/weather";
-  const query = `q=${city}&appid=${WEATHER_API_KEY}`;
-  return fetchData(endpoint, query);
-}
-
-async function fetchIpInfo() {
-  const endpoint = "https://ipinfo.io";
-  const query = `token=${IP_INFO_API_KEY}`;
-  return fetchData(endpoint, query);
-}
+};
 
 async function init() {
-  const { city, country } = await fetchIpInfo();
-  const weatherData = await fetchWeather(`${city}, ${country}`);
+  const fetchIpInfo = fetchData("https://ipinfo.io");
+  const fetchWeather = fetchData(
+    "https://api.openweathermap.org/data/2.5/weather"
+  );
+
+  const { city, country } = await fetchIpInfo(`token=${IP_INFO_API_KEY}`);
+  const weatherData = await fetchWeather(
+    `q=${city}, ${country}&appid=${WEATHER_API_KEY}`
+  );
+
   console.log(weatherData);
 }
 
