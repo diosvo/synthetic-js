a9399f57e062f14560204966f20db6acfd5b38d0
Dios Vo <vtmn1212@gmail.com>
2022-04-08 14:29:59 +0700

    design-pattern (facade)
    - use closures to cache the data

--- design-pattern/facade/weather-ip-info-async-await.js
+++ design-pattern/facade/weather-ip-info-async-await.js
@@ -1,20 +1,47 @@
 const WEATHER_API_KEY = "75d0116a26d6c8be66e9d67612354e27";
 const IP_INFO_API_KEY = "adec6251a9f09e";
 
-const fetchData = (endpoint) => async (query) => {
-  const response = await fetch(`${endpoint}?${query}`);
-
-  if (!response.ok) {
-    throw new Error(`${response.statusText} (${response.status})`);
+const fromCache = (key, cacheInMinutes) => {
+  const cacheInMilliseconds = cacheInMinutes * 60 * 1000;
+  if (localStorage[key] !== undefined) {
+    const cache = JSON.parse(localStorage[key]);
+    if (Date.now() - cache.datetime < cacheInMilliseconds) {
+      return { ...cache.data, cache: cache.datetime + cacheInMilliseconds };
+    }
+    localStorage.removeItem(key);
   }
-  return response.json();
+  return false;
 };
 
+const fetchData =
+  ({ endpoint, cacheInMinutes }) =>
+  async (query) => {
+    const url = `${endpoint}?${query}`;
+    const cache = fromCache(url, cacheInMinutes);
+    if (cache) {
+      return cache;
+    }
+
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`${response.statusText} (${response.status})`);
+    }
+
+    const data = await response.json();
+    localStorage[url] = JSON.stringify({ datetime: Date.now(), data });
+
+    return { ...data, cache: false };
+  };
+
 async function init() {
-  const fetchIpInfo = fetchData("https://ipinfo.io");
-  const fetchWeather = fetchData(
-    "https://api.openweathermap.org/data/2.5/weather"
-  );
+  const fetchIpInfo = fetchData({
+    endpoint: "https://ipinfo.io",
+    cacheInMinutes: 30,
+  });
+  const fetchWeather = fetchData({
+    endpoint: "https://api.openweathermap.org/data/2.5/weather",
+    cacheInMinutes: 10,
+  });
 
   const { city, country } = await fetchIpInfo(`token=${IP_INFO_API_KEY}`);
   const weatherData = await fetchWeather(
