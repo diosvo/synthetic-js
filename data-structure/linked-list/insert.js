b6e2c7431818889825c20e3aea9c0892c8c4f776
Dios Vo <vtmn1212@gmail.com>
2025-04-06 20:53:06 +0700

    dsa (linked list)
    11 - remove
    12 - insert

---
+++ data-structure/linked-list/insert.js
@@ -0,0 +1,45 @@
+/** 🔁 Iterative
+ *
+ * @description Insert a new node with the value into the list at the specified index.
+ *
+ * - n = # of list
+ *
+ * @return The head of the modified linked list
+ *
+ * ⏱️ Time: O(n) - 🚀 Space: O(1)
+ */
+const insertNode = (head, value, index) => {
+  // Edge case: Insert the first/last node.
+  if (index === 0) {
+    const newHead = new Node(value);
+    newHead.next = head;
+    return newHead;
+  }
+
+  let counter = 0;
+  let current = head;
+
+  while (current !== null) {
+    // Insertion point to stop to add the new node
+    if (counter === index - 1) {
+      const temp = current.next;
+      current.next = new Node(value);
+      // Chain the remaining nodes
+      current.next.next = temp;
+    }
+
+    counter++;
+    current = current.next;
+  }
+
+  return head;
+};
+
+// When counter = index - 1
+// 0      ->      1      ->      2      ->      3
+// A      ->      B      ->      C      ->      D
+//                current        temp
+
+// After insertion
+// 0      ->      1      ->      2      ->      3      ->      4
+// A      ->      B      ->      X      ->      C      ->      D
---
+++ data-structure/linked-list/remove.js
@@ -0,0 +1,61 @@
+/** 🔆 Idea
+ * 1. Create a prev pointer to track the previous node.
+ * 2. Traverse the linked list:
+ *    - current.val == target -> prev.next = current.next
+ *    - set prev to current
+ * 4. Edge cases: Remove head/tail node.
+ *
+ * Do it in place.
+ */
+
+/** 🔁 Iterative
+ *
+ * @description Delete the node containing the target value.
+ *
+ * - n = # of list
+ *
+ * @return The head of the modified linked list
+ *
+ * ⏱️ Time: O(n) - 🚀 Space: O(1)
+ */
+const removeNode = (head, target) => {
+  // Edge case: Remove the first/last node.
+  if (head.val === target) return head.next;
+
+  let prev = null;
+  let current = head;
+
+  while (current !== null) {
+    if (current.val === target) {
+      prev.next = current.next;
+      break;
+    }
+
+    prev = current;
+    current = current.next;
+  }
+
+  return head;
+};
+
+// ·    -> A      ->      B      ->      C      ->      D
+// prev -> current                       target
+//         prev    ->     current
+//                        prev   ->      current
+
+/** 🔄 Recursive
+ *
+ * - n = # of list
+ *
+ * ⏱️ Time: O(n) - 🚀 Space: O(n)
+ */
+// const removeNode = (head, target) => {
+//   // Base case: if the list is empty
+//   if (head === null) return null;
+
+//   // Recursive case: traverse the list
+//   head.next = removeNode(head.next, target);
+
+//   // If the current node's value matches the target, skip it
+//   return head.val === target ? head.next : head;
+// };
