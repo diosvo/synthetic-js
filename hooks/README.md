19307a81ef4453e17ea90a7b185381a8df2eec7b
Dios Vo <vtmn1212@gmail.com>
2025-04-20 14:54:13 +0700

    hooks: use-counter

---
+++ hooks/README.md
@@ -0,0 +1,5 @@
+# 🎉 Tips and Tricks
+
+- Memoize functions to avoid unnecessary re-renders using [`useCallback`](https://react.dev/reference/react/useCallback).
+
+- Use an [updater function](https://react.dev/reference/react/useState#updating-state-based-on-the-previous-state) to calculate the new value based on the previous value.
--- hooks/use-boolean.js
+++ hooks/use-boolean.js
@@ -2,14 +2,9 @@ import { useCallback, useState } from 'react';
 
 /**
  * @description An optimized version of the useBoolean hook.
- *
- * @param {boolean} initialValue
- * @return {Object} memoized methods (the same function instance is returned across re-renders)
  */
 export default function useBoolean(initialValue = false) {
   const [value, setValue] = useState(initialValue);
-
-  // 💡 Memoize functions to avoid unnecessary re-renders
 
   const setTrue = useCallback(() => {
     setValue(true);
