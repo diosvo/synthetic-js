ce33720908d68b95374601db3a4610af56f9bdef
Dios Vo <vtmn1212@gmail.com>
2025-07-28 23:34:53 +0700

    dsa (graph): has cycle
    technique: cycle detection w/ white-gray-black + floyd

---
+++ data-structure/graph/has-cycle.js
@@ -0,0 +1,59 @@
+/**
+ * ⏱️ Time: O(n) - 🚀 Space: O(e)
+ *
+ * @returns {boolean} true if the graph has a cycle, false otherwise.
+ */
+const hasCycle = (graph) => {
+  const visited = new Set();
+  const visiting = new Set();
+
+  for (const node in graph) {
+    if (cycleDetect(graph, node, visiting, visited)) return true;
+  }
+
+  return false;
+};
+
+const cycleDetect = (graph, node, visiting, visited) => {
+  // Base case
+  // [1.1] Visited has node
+  if (visited.has(node)) return false;
+  // [1.2] Visiting has node - meaning "cycle"
+  if (visiting.has(node)) return true;
+
+  // [2] 📍 Make the current node as in progress
+  visiting.add(node);
+
+  // [3] Traverse neighbors' node
+  for (const neighbor of graph[node]) {
+    if (cycleDetect(graph, neighbor, visiting, visited)) return true;
+  }
+
+  // [4] Mark it as visited
+  visiting.delete(node);
+  visited.add(node);
+
+  return false;
+};
+
+hasCycle({
+  a: ['b'],
+  b: ['c'],
+  c: ['a'],
+}); // -> true
+hasCycle({
+  a: ['b', 'c'],
+  b: ['c'],
+  c: ['d'],
+  d: [],
+}); // -> false
+hasCycle({
+  q: ['r', 's'],
+  r: ['t', 'u'],
+  s: [],
+  t: [],
+  u: [],
+  v: ['w'],
+  w: [],
+  x: ['w'],
+}); // -> false
---
+++ techniques/cycle-detection.md
@@ -0,0 +1,21 @@
+# Cycle Detection
+
+## ▫️▫️◾️ White-Grey-Black
+
+- White: Unexplored
+- Grey: Visiting
+- Black: Visited
+
+### 👾 Practices
+
+1. Graph
+
+   - [Has Cycle](../data-structure/graph/has-cycle.js)
+
+## Floyd
+
+### 👾 Practices
+
+1. Linked List Cycle Detection
+   - [Middle Value](../data-structure/linked-list/middle-value.js)
+   - [Cycle](../data-structure/linked-list/cycle.js)
