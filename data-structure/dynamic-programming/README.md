9718df141c7b701cbcd01859b3e57c069de361c9
Dios Vo <vtmn1212@gmail.com>
2025-08-01 22:09:07 +0700

    dsa (dyprog): fib

---
+++ data-structure/dynamic-programming/README.md
@@ -0,0 +1,10 @@
+# Dynamic Programming
+
+⛑️ Solution: Memoization w/ Recursion
+
+🤔 Memoization like a stack, it remembers node within a value.
+
+## Easy to Hard
+
+1. [fib](./fib.js)
+2. [tribonacci](./tribonacci.js)
---
+++ data-structure/dynamic-programming/fib.js
@@ -0,0 +1,28 @@
+/**
+ * @argument {number} n - A number
+ *
+ * ⏱️ Time: O(n) - 🚀 Space: O(n)
+ *
+ * @returns {number} the  n-th Fibonacci number sequence.
+ */
+function fib(n, memo = {}) {
+  // Check if the result is already computed
+  if (n in memo) return memo[n];
+
+  // Base cases
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  // Not in memo, compute it
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  return memo[n];
+}
+
+fib(0); // -> 0
+fib(1); // -> 1
+fib(2); // -> 1
+fib(3); // -> 2
+fib(4); // -> 3
+fib(5); // -> 5
+fib(35); // -> 9227465
+fib(46); // -> 1836311903
