8437e7874e1db3c3b6a55399c7a295359e637099
Dios Vo <vtmn1212@gmail.com>
2025-08-20 20:03:50 +0700

    dsa: stack

---
+++ data-structure/core/stack.js
@@ -0,0 +1,56 @@
+class Stack {
+  constructor() {
+    this.data = [];
+  }
+
+  /**
+   * Pushes an item onto the top of the stack.
+   * @param {*} item The item to be pushed onto the stack.
+   * @return {number} The new length of the stack.
+   */
+  push(item) {
+    return this.data.push(item);
+  }
+
+  /**
+   * Remove an item at the top of the stack.
+   * @return {*} The item at the top of the stack if it is not empty, `undefined` otherwise.
+   */
+  pop() {
+    return this.data.pop();
+  }
+
+  /**
+   * Determines if the stack is empty.
+   * @return {boolean} `true` if the stack has no items, `false` otherwise.
+   */
+  isEmpty() {
+    return this.data.length < 1;
+  }
+
+  /**
+   * Returns the item at the top of the stack without removing it from the stack.
+   * @return {*} The item at the top of the stack if it is not empty, `undefined` otherwise.
+   */
+  peek() {
+    return this.data[this.length() - 1];
+  }
+
+  /**
+   * Returns the number of items in the stack.
+   * @return {number} The number of items in the stack.
+   */
+  length() {
+    return this.data.length;
+  }
+}
+
+const stack = new Stack();
+stack.isEmpty(); // true
+stack.push(1);
+stack.push(2);
+stack.length(); // 2
+stack.push(3);
+stack.peek(); // 3
+stack.pop(); // 3
+stack.isEmpty(); // false
