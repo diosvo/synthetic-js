a3f8e6e6d51d476435b0983b3405ffb435de136f
Dios Vo <vtmn1212@gmail.com>
2025-08-28 23:43:16 +0700

    dsa: queue

---
+++ data-structure/core/queue.js
@@ -0,0 +1,65 @@
+class Queue {
+  constructor() {
+    this.data = [];
+  }
+
+  /**
+   * Adds an item to the back of the queue.
+   * @param {*} item The item to be pushed onto the queue.
+   * @return {number} The new length of the queue.
+   */
+  enqueue(item) {
+    return this.data.push(item);
+  }
+
+  /**
+   * Removes an item from the front of the queue.
+   * @return {*} The item at the front of the queue if it is not empty, `undefined` otherwise.
+   */
+  dequeue() {
+    return this.data.shift();
+  }
+
+  /**
+   * Determines if the queue is empty.
+   * @return {boolean} `true` if the queue has no items, `false` otherwise.
+   */
+  isEmpty() {
+    return this.length() === 0;
+  }
+
+  /**
+   * Returns the item at the front of the queue without removing it from the queue.
+   * @return {*} The item at the front of the queue if it is not empty, `undefined` otherwise.
+   */
+  front() {
+    return this.isEmpty() ? undefined : this.data[0];
+  }
+
+  /**
+   * Returns the item at the back of the queue without removing it from the queue.
+   * @return {*} The item at the back of the queue if it is not empty, `undefined` otherwise.
+   */
+  back() {
+    return this.isEmpty() ? undefined : this.data[this.data.length - 1];
+  }
+
+  /**
+   * Returns the number of items in the queue.
+   * @return {number} The number of items in the queue.
+   */
+  length() {
+    return this.data.length;
+  }
+}
+
+const queue = new Queue();
+queue.isEmpty(); // true
+queue.enqueue(1);
+queue.enqueue(2);
+queue.length(); // 2
+queue.enqueue(3);
+queue.front(); // 1
+queue.back(); // 3
+queue.dequeue(); // 1
+queue.isEmpty(); // false
