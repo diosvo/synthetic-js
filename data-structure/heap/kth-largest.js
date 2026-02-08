3940574d889b50290e0d7137e1da40f43f6296b6
Dios Vo <vtmn1212@gmail.com>
2026-02-08 16:23:10 +0700

    dsa (heap): kth-largest

---
+++ data-structure/heap/kth-largest.js
@@ -0,0 +1,51 @@
+import HeapDeletion from './heap-deletion';
+
+/**
+ * ⏱️ Time: O(n*log(n)) - 🚀 Space: O(n)
+ *
+ * @returns The kth largest number in the given array.
+ */
+const kthLargest = (numbers, k) => {
+  const sortedArr = numbers.sort((x, y) => Number(x) - Number(y));
+  return sortedArr.at(-k);
+};
+
+/**
+ * ⏱️ Time: O(n*log(k)) - 🚀 Space: O(k)
+ *
+ * @returns The kth largest number in the given array.
+ */
+function kthLargest(numbers, k) {
+  const heap = new HeapDeletion(); // as min heap
+
+  for (number of numbers) {
+    heap.insert(numbers);
+
+    // If the heap size exceeds k, remove the minimum value (root)
+    if (heap.size() > k) {
+      heap.extractMin();
+    }
+  }
+
+  // Return the root
+  return heap.extractMin();
+}
+
+kthLargest([9, 2, 6, 6, 1, 5, 8, 7], 3); // -> 7
+kthLargest([9, 2, 6, 6, 1, 5, 8, 7], 4); // -> 6
+kthLargest([9, 2, 6, 6, 1, 5, 8, 7], 5); // -> 6
+kthLargest([10, 1, 8, 5, 2, 4], 2); // -> 8
+kthLargest(
+  [
+    4, 5, 85, 77, 47, 80, 37, 42, 3, 6, 62, 33, 69, 68, 16, 20, 83, 39, 14, 58,
+    75, 35, 72, 36, 19, 18, 66, 61, 41, 79, 28, 43, 7, 24, 40, 53, 32, 12,
+  ],
+  9,
+); // -> 68
+kthLargest(
+  [
+    4, 5, 85, 77, 47, 80, 37, 42, 3, 6, 62, 33, 69, 68, 16, 20, 83, 39, 14, 58,
+    75, 35, 72, 36, 19, 18, 66, 61, 41, 79, 28, 43, 7, 24, 40, 53, 32, 12,
+  ],
+  9,
+); // -> 68
